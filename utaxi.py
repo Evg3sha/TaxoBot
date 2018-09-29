edc6a843105e3980184841134e611d187f0174ea
Evgenia Sizova <evgeshamsk92@gmail.com>
2018-09-29 15:55:14 +0300

    Added module utaxi

--- utaxi.py
+++ utaxi.py
@@ -9,21 +9,24 @@ def __create_client():
     return client
 
 
-def get_region_info(long, lat):
+def get_region_info(lat, long):
     client = __create_client()
     response = client.get_products(lat, long)
     print(response.status_code)
     return response.json
 
 
-def get_ride_cost(long_from, lat_from, long_to, lat_to):
+def get_ride_cost(lat_from, long_from, lat_to, long_to):
     client = __create_client()
     response = client.get_price_estimates(lat_from, long_from, lat_to, long_to)
     return response.json
 
 
-print(get_region_info(40.690207, -73.966176))  # , 55.696461, 37.771516
-# price = info['options']
-# for pri in price:
-#    price_name = pri['price']
-#    print('Price: {}'.format(price_name))
+# 40.730209, -73.979612, 55.696461, 37.771516
+info = get_region_info(40.730209, -73.979612)
+products = info['products'][1]
+print(products['upfront_fare_enabled'])
+
+info2 = get_ride_cost(42.384316, -83.101800, 42.421465, -83.270715)
+price = info2['prices'][1]
+print(price['estimate'])
