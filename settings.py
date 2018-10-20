62c031917c1264dd2ea93f373b3a61ad681a9824
Nikita Stepanov <casualproc@gmail.com>
2018-10-20 17:03:59 +0300

    like it was

--- settings.py
+++
@@ -1,5 +0,0 @@
-API_KEY = ('654148912:AAHpi7o2PulM7KhL-UUl4dskKEDeA3zNI9I')
-PROXY = { 'proxy_url': 'socks5://t1/learn/python.ru:1080','urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-YA_API_KEY = ('2598e1bc46ce47f699fd2207908d4865')
-YA_CLID = ('ak393')
-LOCATION = '813476c1-e8bb-494a-a0a6-6ee45d5f5e8b'
