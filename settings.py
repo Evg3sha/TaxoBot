f8a44eb77f372d388f5eeb89736f5ae664139159
Evgenia Sizova <evgeshamsk92@gmail.com>
2018-10-25 02:34:36 +0300

    commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+         'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+API_KEY = '652976616:AAG1MWiOaV36x8J7N6bo9GCdzPmRBBzZKMc'
+
+YA_CLID = 'ak393'
+YA_API_KEY = '2598e1bc46ce47f699fd2207908d4865'
+
+U_CLIENT_ID = '9FHJVeB6Y3vtnVGJ-6uCVFCpCBwpwglV'
+U_CLIENT_SECRET = 'lrpgK4eT_ChtXXPdh7PSfNASxeAqKxRL6dqXuKg0'
+U_TOKEN = 'wTIIiCBy5UF1ompRTmwrp1BUJdnmfkKO81DcDTr_' # 'Uw6D3kUMHnH0RQZpNBKez63T-xfdikT684oFmJ_o'
+
+LOCATION = '813476c1-e8bb-494a-a0a6-6ee45d5f5e8b'
